71a46668bce1a81a694e41f11e5ca3b241405d8b
Tirth <148422031+Tirthx2006@users.noreply.github.com>
2026-02-22 16:49:08 +0530

    Updated index.js

--- backend/index.js
+++ backend/index.js
@@ -11,9 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB
-mongoose.connect(
-  "process.env.MONGO_URI"
-)
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
